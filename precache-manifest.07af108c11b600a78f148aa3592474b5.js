9548deae1fed99f5e3d9a0b495566baf963cdfb4
Chan Jian Hui Jonathan <tmxistz@gmail.com>
2018-10-30 14:46:25 +0900

    Updates

---
+++ precache-manifest.07af108c11b600a78f148aa3592474b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "07f6a687811742d0586c",
+    "url": "/static/js/main.07f6a687.chunk.js"
+  },
+  {
+    "revision": "a700ff875c24dcc7b8f8",
+    "url": "/static/js/1.a700ff87.chunk.js"
+  },
+  {
+    "revision": "07f6a687811742d0586c",
+    "url": "/static/css/main.746b1a3e.chunk.css"
+  },
+  {
+    "revision": "1a201c648778f946716baefe3dc25d61",
+    "url": "/index.html"
+  }
+];
